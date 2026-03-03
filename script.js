2acd0391b6e9e61726daa1faca4e9811896e5d9d
Alyona Belova <55944529+alyona-belova@users.noreply.github.com>
2026-03-03 13:08:14 +0300

    fix: image size

--- script.js
+++ script.js
@@ -134,8 +134,8 @@ document.getElementById("downloadBtn")?.addEventListener("click", function () {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
-  canvas.width = 800;
-  canvas.height = 800;
+  canvas.width = 300;
+  canvas.height = 300;
 
   ctx.fillStyle = "#FFFFFF";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
